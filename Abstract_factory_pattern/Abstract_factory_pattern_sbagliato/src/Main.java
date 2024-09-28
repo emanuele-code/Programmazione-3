a0de35fdf33f7b8531d1f4ece1e9dd75acd83c17
emanule-code <emanuele.pacilio0@gmail.coim>
2024-09-28 18:34:57 +0200

    Stesura dell'Abstract Factory Method, cartella sbagliata spiega il perchè ci serve e cosa non fare, quella corretta è la reale implementazione del pattern

---
+++ Abstract_factory_pattern/Abstract_factory_pattern_sbagliato/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Ristorante panino = new BibiteFactory();
+        Alimento paninoCarne = panino.ordinaAlimento("Acqua"); // qui passo cìò che voglio
+
+    }
+}
---
+++ Factory_pattern/simple_factory_pattern/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ristorante ristorante = new Ristorante();
+        Panino panino = ristorante.ordinaPanino("pesce");
+
+    }
+}
