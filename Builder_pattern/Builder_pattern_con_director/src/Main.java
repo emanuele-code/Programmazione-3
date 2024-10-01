4e638f1b8c32ab0e922acfa35f2632ee65c3c484
emanule-code <emanuele.pacilio0@gmail.coim>
2024-10-01 21:48:11 +0200

    riscrittura del builder pattern senza director, e definizione di quello con director

---
+++ Builder_pattern/Builder_pattern_con_director/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ComputerBuilder pc = new ComputerBuilder(); // istanzio l computerBuilder che verrà passato al costruttore della classe director
+        ComputerDirector director = new ComputerDirector(pc); // istanzio la classe director
+        ComputerProduct prodotto = director.fasciaAlta(); // accedo al metodo della classe director che ritorna il prodotto
+        System.out.println(prodotto);
+
+        ComputerProduct prodotto2 = new ComputerBuilder().prezzo(3000).build(); // posso continuare ad usarlo come se non avessi la director
+        System.out.println(prodotto2);
+    }
+}
